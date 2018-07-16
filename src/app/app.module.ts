41b9a89327bdfa986b334c06bd4a178ee0f02db8
4thguy <ivanxuereb@gmail.com>
2018-07-16 11:38:14 +0200

    Integrate into the application the Angular Flex Layout library (OPTIONAL)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule, MatToolbarModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -16,12 +17,14 @@ import { MatButtonModule, MatCheckboxModule, MatToolbarModule } from '@angular/m
     MatButtonModule,
     MatCheckboxModule,
     MatToolbarModule,
+    FlexLayoutModule,
   ],
   exports: [
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
     MatToolbarModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
